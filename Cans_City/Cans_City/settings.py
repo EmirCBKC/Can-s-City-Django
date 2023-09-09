06fb718083f0e0de7e6b6f94f952fcd3098ed6a7
Emircan ÇUBUKCU <emircancbkc@gmail.com>
2023-09-09 15:16:39 +0300

    media-url-root-add

--- Cans_City/Cans_City/settings.py
+++ Cans_City/Cans_City/settings.py
@@ -117,9 +117,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT=os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/upload/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'upload')
