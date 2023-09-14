44fc9970dc7259ad5f0145f2c77b4a554a302333
Emircan ÇUBUKCU <emircancbkc@gmail.com>
2023-09-14 11:00:25 +0300

    js-update

--- Cans_City/appMy/static/js/app.js
+++ Cans_City/appMy/static/js/app.js
@@ -8,27 +8,31 @@ let user = document.querySelector("#user");
 let userContent = document.querySelector(".user-content");
 userContent.style.display = "none";
 
-basket.addEventListener("click", () => {
+function basketClick() {
     if (basketContent.style.display === "none") {
         basketContent.style.display = "block";
-        userContent.style.display = "none";
+        if (userContent) {
+            userContent.style.display = "none";
+        }
     } else {
         basketContent.style.display = "none";
     }
-});
+}
 
-user.addEventListener("click", () => {
+function userClick() {
     if (userContent.style.display === "none") {
         userContent.style.display = "block";
-        basketContent.style.display = "none";
+        if (basketContent) {
+            basketContent.style.display = "none";
+        }
     } else {
         userContent.style.display = "none";
     }
-});
+}
 
 //! PROFILE => profile edit
-function aktiflestir(){
-    document.querySelectorAll(".edit-profil").forEach((i)=>{i.disabled=false});
-    document.querySelector(".save").style.display="block";
-    document.querySelectorAll(".profile-img-save").forEach((i)=>{i.style.display="block"});
+function aktiflestir() {
+    document.querySelectorAll(".edit-profil").forEach((i) => { i.disabled = false });
+    document.querySelector(".save").style.display = "block";
+    document.querySelectorAll(".profile-img-save").forEach((i) => { i.style.display = "block" });
 }
